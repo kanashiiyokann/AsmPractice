ff09868f588b4c2c145cb80834f871dd67489342
zhaoyouwen <zhaoyouwen@dgg.net>
2019-12-13 17:30:49 +0800

    1

---
+++ src/main/java/yayi/practice/modules/user/service/UserService.java
@@ -0,0 +1,7 @@
+package yayi.practice.modules.user.service;
+
+import yayi.practice.modules.user.entity.User;
+
+public interface UserService {
+    void save(User user);
+}
---
+++ src/main/java/yayi/practice/modules/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package yayi.practice.modules.user.service.impl;
+
+import yayi.practice.modules.user.entity.User;
+import yayi.practice.modules.user.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    public void save(User user) {
+        //todo  save the given user
+    }
+}
